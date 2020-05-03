62060042eeba8f95dbb0d4f23939b407c573568c
Aizada Azirbayeva <azirbayeva@Aizadas-MBP.fios-router.home>
2020-05-02 22:32:59 -0400

    Save

--- Aiza's Bakery-logo/sticynav.js
+++ Aiza's Bakery-logo/sticynav.js
@@ -5,6 +5,7 @@ var sticky = navbar.offsetTop;
 
 function navStickFunction() {
     if (window.pageYOffset >= sticky) {
+        //alert('high' + document.getElementsByClassName("navbar").length + " -> " + sticky)
         navbar.classList.add("sticky")
     } else {
         navbar.classList.remove("sticky");
