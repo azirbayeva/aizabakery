9bd62a88343e9b800ff6933ce2c75f28a17e6db3
Aizada Azirbayeva <azirbayeva@Aizadas-MBP.fios-router.home>
2020-05-03 20:43:15 -0400

    Save

---
+++ Aiza's Bakery-logo/query.js
@@ -0,0 +1,24 @@
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return undefined;
+}
+var isComingFromIndex = getQueryVariable('fromIndex')
+var isFormHidden = false
+if (isComingFromIndex != undefined) {
+    var f = document.getElementById('feedbackForm')
+    if (f != undefined) {
+        isFormHidden = true
+        f.remove()
+    }
+}
+
+if (!isFormHidden) {
+    var f = document.getElementById('lookingForward')
+    f.remove()
+}
