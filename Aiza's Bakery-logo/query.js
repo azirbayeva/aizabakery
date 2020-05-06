4813dea2f286f09d61d6e7501653bbab724774d7
Aizada Azirbayeva <azirbayeva@Aizadas-MBP.fios-router.home>
2020-05-05 22:45:20 -0400

    all good

--- Aiza's Bakery-logo/query.js
+++ Aiza's Bakery-logo/query.js
@@ -4,6 +4,7 @@ function getQueryVariable(variable)
        var vars = query.split("&");
        for (var i=0;i<vars.length;i++) {
                var pair = vars[i].split("=");
+               //alert("hi")
                if(pair[0] == variable){return pair[1];}
        }
        return undefined;
